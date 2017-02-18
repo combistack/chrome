eb2d491eeca1c82bbfb0d67a7b86aeb5e49e7527
Nemanja Nedeljković <nemanjan00@gmail.com>
2017-02-18 21:48:04 +0100

    Path

--- example.js
+++ example.js
@@ -1,8 +1,12 @@
+// Print facebook cookies
+
 require("./index.js")("~/.config/google-chrome/Default").cookies.site.getCookies("facebook.com").then(function(data){
 	console.log(data);
 });
 
+// Pront all cookies
+
 require("./index.js")("~/.config/google-chrome/Default").cookies.all.getCookies().then(function(data){
 	console.log(data);
 });
-;
+
